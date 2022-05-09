c4a10d0967893aab556c76720c96804dadf9bb42
dandyandy22 <andrasm96@gmail.com>
2022-05-08 18:45:20 -0700

    Main: Introduce Button
    Introduce new class Button and
    pass it into draw_button.

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ## Smells
 * Long Parameter List - paint & draw_button both take in five parameters
 * Comments - each conditional has a description
+* Duplicated Code - paint is past nearly identical parameters for both conditions
 
 ## Refactorings
+* Extract Method
 * Remove Flag Argument
 * Introduce Parameter Object
 
--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
+class Button
+  
+  def initialize(label_text, x, y, foreground_color)
+    @label_text = label_text
+    @x = x
+    @y = y
+    @foreground_color = foreground_color
+  end
+end
 
-
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(label_text, x, y, foreground_color, is_dark_mode, button)
   if is_dark_mode
     # darken foreground color for dark mode
     paint(label_text, x, y, foreground_color - 10, '#111111')
