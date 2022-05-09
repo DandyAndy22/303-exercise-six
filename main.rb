8a9374b2206494343ba00802503d1696809732ff
dandyandy22 <andrasm96@gmail.com>
2022-05-08 18:51:29 -0700

    Main: Implement Button
    Create an instance of the button class,
    Pass it into draw_button and
    replace original params with calls to
    button attributes.

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
-class Button
-  
+class Button  
   def initialize(label_text, x, y, foreground_color)
     @label_text = label_text
     @x = x
@@ -8,12 +7,14 @@ class Button
   end
 end
 
-def draw_button(label_text, x, y, foreground_color, is_dark_mode, button)
+button = Button.new("Submit", 15, 25, 252525)
+
+def draw_button(button, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
   end
 end
